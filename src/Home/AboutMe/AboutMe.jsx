c0a6da6da9434e7c48b9d4c03e53fe97d2558339
treasurejeffy <treasurejeffy@gmail.com>
2024-07-15 05:59:32 -1100

    update in work

--- src/Home/AboutMe/AboutMe.jsx
+++ src/Home/AboutMe/AboutMe.jsx
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col, Button} from "react-bootstrap";
 import css from '../AboutMe/AboutMe.module.css'
 import treasure from '../AboutMe/treasure.png';
-import {  Heart, Check, PersonBadge, ArrowLeft } from 'react-bootstrap-icons';
+import {  Heart, Check, PersonBadge, ArrowLeft, BoundingBox } from 'react-bootstrap-icons';
 
 export default function AboutMe() {
     
@@ -15,16 +15,16 @@ export default function AboutMe() {
                         <img src={treasure} alt="Me treasure" className={`${css.treasureImg} img-fluid rounded`} />
                     </Col>
                     <Col className={`${css.headText}`} >
-                        <h2 className='mt-5  text-center'><span className='text-success '>*</span>About Me</h2>
-                        <Row> 
-                            <article className='text-center pb-3'>Treasure Japheth, a Nigerian-based web developer with a passion for creating unique and innovative digital experiences with 2 years + in the field. With a background in [<span>RAD5 Tech Hub Aba.</span>], I bring a blend of technical skills and creative thinking to every project.</article>
-                            <Col className=' mt-1'> 
-                                <h5 className='fw-bolder'><Heart className='text-success fs-4' /> What I Love To Do</h5>
-                                <p className='fw-lighter'> I thrive on turning ideas into reality through the language of code. My journey in the world of web development has been exciting, and I constantly seek new challenges to expand my skill set. I specialize in Frontend Development.</p>
+                        <h2 className='text-center'><span className='text-success '>*</span>About Me</h2>
+                        <article className='text-center pb-3'>Treasure Japheth, a Nigerian-based web developer with a passion for creating unique and innovative digital experiences with 2 years + in the field. With a background in [<span>RAD5 Tech Hub Aba.</span>], I bring a blend of technical skills and creative thinking to every project.</article>
+                        <Row xl={2} lg={2} md={2} sm={1} xs={1}>                             
+                            <Col className=' mt-1 text-start'> 
+                                <h5 className='fw-semibold fs-5'><Heart className='text-success fs-4 me-2' /> What I Love To Do</h5>
+                                <p className='pt-2 '> I thrive on turning ideas into reality through the language of code. My journey in the world of web development has been exciting, and I constantly seek new challenges to expand my skill set. I specialize in Frontend Development.</p>
                             </Col>
-                            <Col className=' mt-2'>
-                                <h5 className='fw-bolder'><PersonBadge className='text-success fs-4' /> My Approach</h5>
-                                <p className='fw-lighter pt-2'> My approach to web development is mainly for users. I believe in creating websites and applications that not only look good but also provide a seamless and enjoyable experience. I take pride in delivering clean and efficient code.</p>
+                            <Col className=' mt-2 text-end'>
+                                <h5 className='fw-semibold fs-5'><BoundingBox className='text-success fs-4 me-2' /> My Approach</h5>
+                                <p className='pt-2'> My approach to web development is mainly for users. I believe in creating websites and applications that not only look good but also provide a seamless and enjoyable experience. I take pride in delivering clean and efficient code.</p>
                             </Col>
                         </Row>
                         <div className='d-flex justify-content-end'>
--- src/Home/contactMe/contactMe.jsx
+++ src/Home/contactMe/contactMe.jsx
@@ -11,21 +11,21 @@ import { useEffect, useRef } from "react";
 import emailjs from '@emailjs/browser';
 
 export default function ContactMe() {
-    const [cltName, setCltName] = useState('');
-    const [cltEmail, setCltEmail] = useState('');
-    const [cltNumber, setCltNumber] = useState();
-    const [cltComment, setCltComment] = useState('');
     let [saveData, setSaveData] = useState([]);
     let notify;
     const [currentDateTime, setCurrentDateTime] = useState('');
-    let save = [
-    {
-    Name: cltName,
-    email: cltEmail,
-    phoneNo: cltNumber,
-    comments: cltComment
-    }
-    ]
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        message: '',
+        phoneNo: ''
+    });
+    const [loading, setLoading] = useState(false);
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
+
     useEffect(() => {
       // Function to get the current date and time
       const getCurrentDateTime = () => {
@@ -44,6 +44,7 @@ export default function ContactMe() {
       // Clean up interval on component unmount
       return () => clearInterval(interval);
     }, []);
+
     const [clientList,setClientList ]= useState ([])
     const getClientsCollectionRef = collection(db,'clientsData')
 
@@ -56,7 +57,7 @@ export default function ContactMe() {
                 ...doc.data(),
                  id: doc.id,
             }))
-            console.log(filter)
+            // console.log('Data:',filter)
         } catch(err){
             console.error(err)
         }
@@ -67,35 +68,43 @@ export default function ContactMe() {
         setSaveData([])
     },[])
 
-
-
-
-    let thanks= 'Thanks you I will get back to you through the Email!'
-
+    let thanks= `Thanks you I will get back to you through the Email.`
     const handleSubmit = async (e) =>{
         e.preventDefault();
-        notify = toast(<b className={`${Css.notify} text-success`}>{thanks}</b>) ;
+        setLoading(true);
         try {
-            await addDoc(getClientsCollectionRef, {Name: cltName, email: cltEmail, phoneNo: cltNumber, comments: cltComment});            
+            await addDoc(getClientsCollectionRef, formData);            
             getClientsList();
-            emailjs
-            .sendForm('service_ngxlbie', 'template_t75y0se', e.target, {
-              publicKey: 'B47fSFKE0sVRzuySp',
-            })
-            setSaveData(...saveData,save)
+  
+             // Using e.target to pass the form element
+            emailjs.sendForm(
+                'service_ngxlbie',  // Replace with your service ID
+                'template_t75y0se',  // Replace with your template ID
+                e.target,         // The form element
+                'B47fSFKE0sVRzuySp'  // Replace with your public key
+            ).then(
+                (response) => {
+                    console.log('SUCCESS!', response.status, response.text);
+                },
+                (error) => {
+                    console.log('FAILED...', error);
+                }
+            );
+            toast.success(thanks);
+            setSaveData(...saveData,formData)
         } catch (error) {
             console.error(error)
-        }
-    }
-
-    
+        }finally {
+            setLoading(false);
+          }
+    }  
     
     return(
         <section className="mt-5">
             <Container>
                 <div className={`${Css.contactHeader}  text-center mb-4`}>
                     <h2 ><span className="text-primary">*</span>Contact Me</h2>
-                    <p>Thank you for visiting my portfolio! If you have any inquiries, collaboration opportunities, or just want to say hello, feel free to reach out to me using any of the following methods: [But remember this is the time <ClockFill className="fs-4 fw-bold text-primary"/> <span className="text-danger">{currentDateTime}</span> for you to put your shop, idea & institution into technology.]</p>
+                    <p>Thank you for visiting my portfolio! If you have any inquiries, collaboration opportunities, or just want to say hello, feel free to reach out to me using any of the following methods: [But remember this is the time <ClockFill className="fs-4 fw-bold text-primary "/> <span className="text-danger pe-auto">{currentDateTime}</span> for you to put your shop, idea & institution into technology.]</p>
                 </div>
                 <Row className=" align-items-center" lg={2} sm={1} xs={1} md={1} >                   
                     <Col> 
@@ -103,17 +112,26 @@ export default function ContactMe() {
                             <Form className="bg-light py-5 px-4 mb-3 border rounded" ref={form}  onSubmit={handleSubmit}>
                                 <h5 className="text-center mb-4"> For inquries pls fill this form!</h5>
                                 <Form.Label>Full Name:</Form.Label>
-                                <Form.Control type="text" placeholder="Enter your full Name " className="mb-4" required onChange={(e) => setCltName(e.target.value)}/>
+                                <Form.Control type="text" placeholder="Enter your full Name " className="mb-4"  name="name" value={formData.name} onChange={handleChange} required/>
                                 <Form.Label>Email Address:</Form.Label>
-                                <Form.Control type="email" placeholder="Enter your email@gmail.com" className="mb-4" required onChange={(e) => setCltEmail(e.target.value)}/>
+                                <Form.Control type="email" placeholder="Enter your email@gmail.com" className="mb-4"  name="email" value={formData.email} onChange={handleChange} required/>
                                 <Form.Label >Phone Number:</Form.Label>
-                                <Form.Control type="number" placeholder="Enter your phone number" required onChange={(e) => setCltNumber(e.target.valueAsNumber)}/>
+                                <Form.Control type="number" placeholder="Enter your phone number"  name="phoneNo" value={formData.phoneNo} onChange={handleChange} required/>
                                 <Form.Group controlId="exampleForm.ControlTextarea1" className="mt-3">
                                     <Form.Label>Explain:</Form.Label>
-                                    <Form.Control as="textarea" rows={4} onChange={(e) => setCltComment(e.target.value)} placeholder="Explain, how I can help pls."/>
+                                    <Form.Control as="textarea" rows={4}  name="message" value={formData.message} onChange={handleChange} required placeholder="Explain, how I can help pls."/>
                                 </Form.Group>
                                 <div className="d-flex justify-content-center">
-                                    <Button type="submit" className="mt-5 d btn btn-primary px-5 py-2 fs-5"><b> Submit</b></Button>
+                                <Button type="submit" disabled={loading} className="mt-5 btn btn-primary px-5 py-2 fs-5">
+                                    {loading ? (
+                                    <>
+                                        <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                                        <span className="ms-2"><b>Loading...</b></span>
+                                    </>
+                                    ) : (
+                                    <b>Submit</b>
+                                    )}
+                                </Button>
                                 </div>
                             </Form>
                         </div>     
@@ -122,7 +140,7 @@ export default function ContactMe() {
                         <div className={`${Css.contactMesocial} rounded border bg-light`}>
                             <h5 className="mb-5 ">Get more closer to me!</h5>
                             <div>
-                                <a href="https://web.facebook.com/treasure.japheth.14"><Button className="px-5 py-3"><Facebook className="mx-2 mb-1"/><b>facebook</b></Button></a>
+                                <a href="https://web.facebook.com/treasure.japheth.14" disabled><Button className="px-5 py-3"><Facebook className="mx-2 mb-1"/><b>facebook</b></Button></a>
                             </div>
                             <div className="mt-4">
                                 <a href="https://twitter.com/JaphethTreasure"><Button className="px-5 py-3"><Twitter className="mx-2 mb-1"/><b className="px-2">Twitter</b></Button></a>
@@ -148,7 +166,7 @@ export default function ContactMe() {
             </Container>
             <ToastContainer
                     position="top-right"
-                    autoClose={5000}
+                    autoClose={10000}
                     hideProgressBar={false}
                     newestOnTop={false}
                     closeOnClick
@@ -157,7 +175,7 @@ export default function ContactMe() {
                     draggable
                     pauseOnHover
                     theme="light"
-                />
+            />
         </section>
     )
 }
